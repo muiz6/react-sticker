1e2c58107a2d2578c01ef16f0bc6f2f43b0d8bc6
muiz6 <muizhassan83@gmail.com>
2023-08-24 15:37:32 +0500

    start

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import { MdSync } from "react-icons/md";
+
+export default function Home() {
+  return (
+    <section>
+      <div className="flex justify-center items-center bg-[url(/assets/bg.webp)] py-20">
+        <div className="border-dashed border-4 border-gray-900 relative">
+          <img src="/assets/psyduck.png" />
+          <div className="bg-white shadow-lg p-2 absolute right-0 bottom-0 translate-x-1/2 translate-y-1/2 rounded-full">
+            <MdSync />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
