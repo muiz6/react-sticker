41702c7cd842ec5436485c75c9249d06fb5a0759
muiz6 <muizhassan83@gmail.com>
2023-09-01 16:58:43 +0500

    fixed btn position

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="flex justify-center items-center bg-[url(/assets/bg.webp)] py-20">
         <div className="border-dashed border-4 border-gray-900 relative">
           <img src="/assets/psyduck.png" />
-          <div className="bg-white shadow-lg p-2 absolute right-0 bottom-0 translate-x-1/2 translate-y-1/2 rounded-full">
+          <div className="bg-white shadow-lg p-2 absolute left-0 bottom-0 -translate-x-1/2 translate-y-1/2 rounded-full">
             <MdSync />
           </div>
         </div>
